75ac99766e865da14ac2d606787bfb22faf8d4bb
webuser <ryan@virtuecenter.com>
2013-09-27 14:42:43 -0400

    basic layout

---
+++ src/Person/Person.php
@@ -0,0 +1,66 @@
+<?php
+namespace Person;
+use Config\Config;
+use DB\Mongo;
+
+class Person {
+	private function create () {
+
+	}
+
+	public static function byId ($id) {
+
+	}
+
+	public static function byEmail ($mail) {
+
+	}
+
+	public function formAdd ($type, $data) {
+
+	}
+
+	public function messageSend (Array $message) {
+
+	}
+
+	public functions attributesSet (Array $attributes) {
+
+	}
+
+	public function passwordSet($password) {
+
+	}
+
+	public function passwordResetRequest () {
+
+	}
+
+	public function classify () {
+
+	}
+
+	public function declassify () {
+
+	}
+
+	public function addressSet(Array $address) {
+
+	}
+
+	public function photoSet(Array $image) {
+
+	}
+
+	public function messageAdd ($message) {
+
+	}
+
+	public function activityAdd($type, $message) {
+
+	}
+
+	public function merge () {
+
+	}
+}
