db5672af9f3ca74d448425c8e6942574401a10ce
webuser <ryan@virtuecenter.com>
2014-02-01 16:21:40 -0500

    syntax error

--- src/Person/Person.php
+++ src/Person/Person.php
@@ -3,32 +3,101 @@ namespace Person;
 
 class Person {
 	private $db;
+	private $id;
+	private $current = false;
 
-	public function __construct ($db) {
+	public function __construct ($db, $config) {
 		$this->db = $db;
+		if (isset($_SESSION['user']) && isset($_SESSION['user']['_id'])) {
+			$this->current = $_SESSION['user']['_id'];
+		}
 	}
 
-	private function create () {
+	public function available () {
+		if (isset($_SESSION['user']) && isset($_SESSION['user']['_id'])) {
+			$this->current = $_SESSION['user']['_id'];
+			return true;
+		}
+		return false;
+	}
+
+	public function create (Array $attributes) {
+		$salt = $config->auth['salt'];
+		if (isset($attributes['password'])) {
+			$attributes['password'] = sha1($salt . 'password');
+		} else {
+			$attributes['password'] = sha1($salt . uniqid());
+		}
+		if (isset($attributes['email'])) {
+			$attributes['email'] = strtolower(trim($attributes['email']));
+		}
+		$id = new \MongoId();
+		$attributes['_id'] = $id;
+		$dbURI = 'users:' . (string)$id;
+		$attributes['created_date'] = new \MongoDate(strtotime('now'));
+		$attributes['_id'] = $id;
+		try {
+			$this->db->documentStage($dbURI, $attributes)->upsert();
+		} catch (\Exception $e) {
+			return 'Error: ' . $e->getMessage();
+		}
+		$_SESSION['user'] = $attributes;
+		return true;
+	}
+
+	public function findById ($id) {
+		$check = $this->db->collection('users')->findOne(['_id' => $this->db->id($id)]);
+		if (isset($check['_id'])) {
+			$this->current = $check['_id'];
+			return $this->current;
+		}
+		return false;
+	}
+
+	public function findByEmail ($email) {
+		$this->db->collection('users')->findOne(['email' => strtolower(trim($email))]);
+		if (isset($check['_id'])) {
+			$this->current = $check['_id'];
+			return $this->current;
+		}
+		return false;
+	}
+
+	public function groupJoin ($group) {
+		if ($this->current === false) {
+			return false;
+		}
+		$this->operation(['$addToSet' => ['groups' => $group]]);
+	}
+
+	private function operation ($operation) {
+		$this->db->collection('users')->update(['_id' => $this->db->id($this->current)], $operation);
+	}
+
+	public function groupLeave ($group) {
+		if ($this->current === false) {
+			return false;
+		}
+		$this->operation(['$pull' => ['groups' => $group]]);
+	}
+
+	public function recordAdd ($dbURI, $description, $unique=false) {
+		if ($unique == true) {
+			if ($this->recordCheck($collection) === false) {
+
+			}
+		}
+	}
+
+	public function recordCheck ($collection) {
 
 	}
 
-	public static function byId ($id) {
+	public function commentAdd ($id) {
 
 	}
 
-	public static function byEmail ($mail) {
-
-	}
-
-	public function formAdd ($type, $data) {
-
-	}
-
-	public function messageSend (Array $message) {
-
-	}
-
-	public functions attributesSet (Array $attributes) {
+	public function attributesSet (Array $attributes) {
 
 	}
 
@@ -40,14 +109,6 @@ class Person {
 
 	}
 
-	public function classify () {
-
-	}
-
-	public function declassify () {
-
-	}
-
 	public function addressSet(Array $address) {
 
 	}
@@ -56,15 +117,22 @@ class Person {
 
 	}
 
-	public function messageAdd ($message) {
+	//notes
+	public function noteAdd ($message) {
 
 	}
 
-	public function activityAdd($type, $message) {
+	//activities
+	public function activityAdd($type, $message, $date=false) {
 
 	}
 
-	public function merge () {
+	//classifications
+	public function classify ($tag) {
+
+	}
+
+	public function declassify ($tag) {
 
 	}
 }
