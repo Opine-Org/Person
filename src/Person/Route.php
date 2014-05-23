6d48fc1a97cf974a1513d56fa69cecfa9e93eb1e
rmahoney-bsd <rmahoney@bluestatedigital.com>
2014-05-23 08:59:28 -0400

    working on activity tracking display

--- src/Person/Route.php
+++ src/Person/Route.php
@@ -5,12 +5,21 @@ class Route {
     private $root;
     private $bundleRoot;
     private $route;
+    private $separation;
 
     public function __construct ($container, $root, $bundleRoot) {
         $this->root = $root;
         $this->bundleRoot = $bundleRoot;
         $this->route = $container->route;
+        $this->separation = $container->separation;
     }
 
-    public function paths () {}
+    public function paths () {
+    	$this->route->get('/Person/stream', function () {
+    		$this->separation->app('bundles/Person/app/collections/activity_stream')->
+    			layout('Person/collections/activity_stream')->
+    			template()->
+    			write();
+    	});
+    }
 }
