a667248d12be6fd6bf0ccd86c05c61f90065e0c1
Ryan Mahoney <ryan@virtuecenter.com>
2014-11-24 15:54:38 -0500

    helper refactor

---
+++ src/Person/helpers/Gravitar.php
@@ -0,0 +1,20 @@
+<?php
+namespace Helper\Person;
+
+class Gravitar {
+    public function render (Array $arguments, Array $options) {
+        if (!isset($options['email'])) {
+            $options['email'] = 'test@email.com';
+        }
+        if (!isset($options['s'])) {
+            $options['s'] = 80;
+        }
+        if (!isset($options['d'])) {
+            $options['d'] = 'mm';
+        }
+        if (!isset($options['r'])) {
+            $options['r'] = 'mm';
+        }
+        return 'https://www.gravatar.com/avatar/' . md5(strtolower(trim($options['email']))) . '?s=' . $options['s'] . '&d=' . $options['d'] . '&r=' . $options['r'];
+    }
+}
